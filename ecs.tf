ca9f839959639d31e2f64b8495f27ee94ec42f74
Guilherme Oliveira <guilhermeoliveira2862@hotmail.com>
2020-07-10 18:27:34 -0300

    bug-fixed: Go App!

--- ecs.tf
+++ ecs.tf
@@ -27,7 +27,7 @@ resource "aws_ecs_task_definition" "app" {
 }
 
 resource "aws_ecs_service" "main" {
-  name            = "cb-app"
+  name            = "cb-service"
   cluster         = aws_ecs_cluster.main.id
   task_definition = aws_ecs_task_definition.app.arn
   desired_count   = var.app_count
